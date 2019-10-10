45801677e88fb63954f91ab4231113e8f2173d7a
mathias brink <mathiasbrink@live.dk>
2019-10-10 10:43:11 +0200

    Add project files.

---
+++ MusicRecordRestPair/Controllers/MusicRecordsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MusicRecordRestPair.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicRecordsController : ControllerBase
+    {
+        private List<MusicRecord> RecordsList = new List<MusicRecord>()
+        {
+            new MusicRecord(1, "nielsen", "den record", 180, 2019),
+            new MusicRecord(2, "birk", "den reco", 120, 2016),
+            new MusicRecord(3, "peter", "den cord", 130, 2017),
+            new MusicRecord(4, "børge", "en record", 140, 2018)
+        };
+
+        // GET: api/MusicRecords
+        [HttpGet]
+        public IEnumerable<MusicRecord> Get()
+        {
+            return RecordsList;
+        }
+
+        // GET: api/MusicRecords/5
+        [HttpGet("{id}", Name = "Get")]
+        public MusicRecord Get(int id)
+        {
+            return RecordsList.Find(i => i.Id == id);
+        }
+
+        // POST: api/MusicRecords
+        [HttpPost]
+        public void Post([FromBody] MusicRecord value)
+        {
+            RecordsList.Add(value);
+        }
+
+        // PUT: api/MusicRecords/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] MusicRecord value)
+        {
+            MusicRecord newRecord = RecordsList.Find(i => i.Id == id);
+            newRecord.Artist = value.Artist;
+            newRecord.Duration = value.Duration;
+            newRecord.Title = value.Title;
+            newRecord.YearOfPublication = value.YearOfPublication;
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            RecordsList.Remove(RecordsList.Find(i => i.Id == id));
+            
+        }
+    }
+}
---
+++ MusicRecordRestPair/MusicRecord.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicRecordRestPair
+{
+    public class MusicRecord
+    {
+        private string _artist;
+        private string _title;
+        private int _duration;
+        private int _yearOfPublication;
+        private int _id;
+
+        public MusicRecord()
+        {
+        }
+
+        public MusicRecord(int id, string artist, string title, int duration, int yearOfPublication)
+        {
+            _id = id;
+            _artist = artist;
+            _title = title;
+            _duration = duration;
+            _yearOfPublication = yearOfPublication;
+        }
+
+        public string Artist
+        {
+            get => _artist;
+            set => _artist = value;
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => _title = value;
+        }
+
+        public int Duration
+        {
+            get => _duration;
+            set => _duration = value;
+        }
+
+        public int YearOfPublication
+        {
+            get => _yearOfPublication;
+            set => _yearOfPublication = value;
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public override string ToString()
+        {
+            return "This is record " + Id;
+        }
+    }
+}
